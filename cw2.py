1a4b4466c884feca2f41fa79607ddaa4d3246c1a
CERB3RUS-88 <cosmiccerberus88@gmail.com>
2022-01-05 22:36:32 +0530

    init

---
+++ cw2.py
@@ -0,0 +1,5 @@
+alpha = input("Enter the character: ")
+if alpha == ("a") or alpha == ("e") or alpha == ("i") or alpha == ("o") or alpha == ("u") or alpha == ("A") or alpha == ("E") or alpha == ("I") or alpha == ("O") or alpha == ("U"):
+    print("It's a vowel")
+else:
+    print("It's a consonant")
---
+++ dsfgsdfgsdfg.py
@@ -0,0 +1,4 @@
+Student_Names = ['Shaurya', 'Monty', 'Pratham', 'Aryan',
+                 'Harshil', 'Anikesh', 'Shashwat', 'Shreyas', 'Tanishq', 'Mona']
+Student_Names.pop(-1)
+print(Student_Names)
