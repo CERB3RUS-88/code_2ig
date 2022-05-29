ca2e4af6f10abd076c6551f09fbabd99eefec724
CERB3RUS-88 <cosmiccerberus88@gmail.com>
2022-05-29 14:25:23 +0530

    init

---
+++ python/HOLIDAY_HW.py
@@ -0,0 +1,43 @@
+# x = int(input("Enter value of x:  "))
+# y = int(input("Enter value of y: "))
+# print(f"value of x is {x}")
+# print(f"value of y is {y}")
+# print("After Swapping:")
+# z = x
+# x = y
+# y = z
+
+# print("Value of x:", x)
+# print("Value of y:", y)
+
+
+# CP = int(input("Enter Cost price:  "))
+# SP = int(input("Enter Selling price:  "))
+# if (CP == SP):
+#     print("There is no loss or profit")
+# elif (CP < SP):
+#     profit = SP-CP
+#     print(f"You have gained profit of {profit} rupees")
+# else:
+#     loss = CP-SP
+#     print(f"You have lost a total of {loss} rupees")
+
+# list1 = ["Audi", "Mercedes", "Tesla", "Jaguar"]
+# print(list1)
+# list1.insert(2, "Hyundai")
+# list1.remove("Jaguar")
+# print(list1)
+
+
+# num1 = int(input("Enter number 1:  "))
+# num2 = int(input("Enter number 2:  "))
+
+# if num1 > num2:
+#     print("Number 1 is the largest")
+# else:
+#     print("Number 2 is the largest")
+
+
+name = input("Enter your name:  ")
+fav_sport = input("Enter your favourite sport:  ")
+print(f"{name} likes to play {fav_sport}")
